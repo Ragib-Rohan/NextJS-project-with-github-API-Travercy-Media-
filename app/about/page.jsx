70ccb431f2d1b014f882076f08213b83da87e9f2
Ragib Rohan <ragibrohan.projects@gmail.com>
2023-04-27 00:11:42 +0600

    !st upload

---
+++ app/about/page.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const AboutePage = () => {
+  return (
+    <div>
+      <h1>AboutePage</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum a
+        optio, facilis maiores eum vitae quod asperiores. Corporis doloremque
+        incidunt maxime doloribus commodi consequatur a, et quae inventore id
+        blanditiis? Provident adipisci natus repellendus. Dolorum saepe id qui
+        iure blanditiis temporibus fugiat aliquid perferendis hic laboriosam
+        possimus quas sed perspiciatis, ab asperiores ducimus enim nihil nostrum
+        tempore numquam, explicabo sapiente?
+      </p>
+    </div>
+  );
+};
+
+export default AboutePage;
---
+++ app/about/team/page.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+export const metadata = {
+  title: "Team page",
+};
+const TeamPage = () => {
+  return (
+    <div>
+      <h1>Team Page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laudantium cum
+        laboriosam illum dolor, exercitationem et adipisci? Dolorum eligendi et
+        illo?
+      </p>
+      <br />
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate,
+        voluptatibus aspernatur. Facilis nihil vero laudantium, nemo maiores
+        minus laborum possimus voluptatum pariatur consectetur quas, sunt
+        commodi eum quo quasi nobis!
+      </p>
+    </div>
+  );
+};
+
+export default TeamPage;
